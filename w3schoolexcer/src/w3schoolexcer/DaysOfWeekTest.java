fb2733fe7baea333a982d1fad2b6d356379b8947
Faisal Iqbal <87160666+iqbalf203@users.noreply.github.com>
2023-06-28 15:20:16 +0530

    Add files via upload

---
+++ w3schoolexcer/src/w3schoolexcer/DaysOfWeekTest.java
@@ -0,0 +1,12 @@
+package w3schoolexcer;
+
+public class DaysOfWeekTest {
+
+	public static void main(String[] args) {
+		
+		DaysOfWeek today = DaysOfWeek.Sunday;
+		System.out.println("Today is: "+today);
+
+	}
+
+}
---
+++ w3schoolexcer/src/w3schoolexcer/ShapeTest.java
@@ -0,0 +1,15 @@
+package w3schoolexcer;
+
+public class ShapeTest {
+
+	public static void main(String[] args) {
+		Shape shape1 = Shape.Hexagon;
+		Shape shape2 = Shape.Circle;
+		
+		System.out.println(shape1);
+		System.out.println(shape2);
+		
+
+	}
+
+}
