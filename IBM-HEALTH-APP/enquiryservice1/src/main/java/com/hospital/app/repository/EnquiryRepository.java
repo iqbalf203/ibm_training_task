2416e747e62b3726b50102e04f700394028f336c
Faisal Iqbal <87160666+iqbalf203@users.noreply.github.com>
2023-08-11 16:47:33 +0530

    Delete IBM-HEALTH-APP/enquiryservice1 directory

--- IBM-HEALTH-APP/enquiryservice1/src/main/java/com/hospital/app/repository/EnquiryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hospital.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hospital.app.model.Enquiry;
-
-public interface EnquiryRepository extends JpaRepository<Enquiry, Long> {
-  
-}
