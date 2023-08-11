ce84fb576b5709e3315ec22296de7538c2f92b33
Faisal Iqbal <87160666+iqbalf203@users.noreply.github.com>
2023-08-11 16:47:44 +0530

    Delete IBM-HEALTH-APP/feedbackservice1 directory

--- IBM-HEALTH-APP/feedbackservice1/src/main/java/com/hospital/app/exception/NotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.hospital.app.exception;
-
-public class NotFoundException extends RuntimeException{
-
-  public NotFoundException(Exception e) {
-    super("Feedback Not Found", e);
-  }
-  
-}
--- IBM-HEALTH-APP/feedbackservice1/src/main/java/com/hospital/app/repository/FeedbackRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hospital.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hospital.app.model.Feedback;
-
-public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
-  
-}
